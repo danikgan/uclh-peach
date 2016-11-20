40396e7cd321d4e75e746ff7b7305cbb48974317
danikgan <danikgan@Daniils-MacBook-Pro.local>
2016-11-20 20:28:37 +0000

    - testing

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	/*----------------------------------- Menu on Mobile Devices -----------------------------------*/
+    $("#top-bar-right-small").click(function(){
+        $(".sub-menu-small").toggle();
+    });
+});

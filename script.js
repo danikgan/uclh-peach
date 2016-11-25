78b8640d70d9ea13812b68902ce3c81fd9c46cb3
danikgan <danikgan@eduroam-int-dhcp-97-96-196.ucl.ac.uk>
2016-11-25 16:15:49 +0000

    - ui finished

--- script.js
+++ script.js
@@ -34,4 +34,120 @@ $(document).ready(function(){
         $(".show-mockup-dg").css("display", "block");
         $(".show-mockup-sm").css("display", "none");
     });
+    
+    
+    $(".mockup-00").click(function(){
+        $(".show-mockup-00").css("display", "block");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        $(".show-mockup-03").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    $(".mockup-01").click(function(){
+        $(".show-mockup-01").css("display", "block");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        $(".show-mockup-03").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    
+    $(".mockup-02").click(function(){
+        $(".show-mockup-02").css("display", "block");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-03").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    $(".mockup-03").click(function(){
+        $(".show-mockup-03").css("display", "block");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    
+    $(".mockup-ho").click(function(){
+        $(".show-mockup-03").css("display", "none");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "block");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    
+    $(".mockup-lo").click(function(){
+        $(".show-mockup-03").css("display", "none");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "block");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    
+    $(".mockup-pa").click(function(){
+        $(".show-mockup-03").css("display", "none");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "block");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    
+    $(".mockup-ps").click(function(){
+        $(".show-mockup-03").css("display", "none");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "block");
+        $(".show-mockup-sm2").css("display", "none");
+    });
+    
+    $(".mockup-sm2").click(function(){
+        $(".show-mockup-03").css("display", "none");
+        $(".show-mockup-00").css("display", "none");
+        $(".show-mockup-01").css("display", "none");
+        $(".show-mockup-02").css("display", "none");
+        
+        $(".show-mockup-ho").css("display", "none");
+        $(".show-mockup-lo").css("display", "none");
+        $(".show-mockup-pa").css("display", "none");
+        $(".show-mockup-ps").css("display", "none");
+        $(".show-mockup-sm2").css("display", "block");
+    });
 });

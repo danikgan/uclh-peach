754b9ba4b0d007c2242a4f9424a0e9cc4316b46c
danikgan <danikgan@eduroam-int-dhcp-97-96-196.ucl.ac.uk>
2016-11-25 14:52:14 +0000

    - style fixes

--- script.js
+++ script.js
@@ -3,4 +3,23 @@ $(document).ready(function(){
     $("#top-bar-right-small").click(function(){
         $(".sub-menu-small").toggle();
     });
+    
+    /*----------------------------------- Open Picture -----------------------------------*/
+    $(".mockup-dg").click(function(){
+        $(".show-mockup-dg").toggle();
+        //$(".mockup-dg").toggle();
+    });
+    $(".show-mockup-dg").click(function(){
+        $(".show-mockup-dg").toggle();
+        //$(".mockup-dg").toggle();
+    });
+    
+    $(".mockup-sm").click(function(){
+        $(".show-mockup-sm").toggle();
+        //$(".mockup-sm").toggle();
+    });
+    $(".show-mockup-sm").click(function(){
+        $(".show-mockup-sm").toggle();
+        //$(".mockup-sm").toggle();
+    });
 });

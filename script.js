d12fee1a2d64ce477e1f806f6ac03052f34796db
danikgan <danikgan@eduroam-int-dhcp-97-96-196.ucl.ac.uk>
2016-11-25 15:45:34 +0000

    - pushing new ui section

--- script.js
+++ script.js
@@ -5,21 +5,33 @@ $(document).ready(function(){
     });
     
     /*----------------------------------- Open Picture -----------------------------------*/
-    $(".mockup-dg").click(function(){
-        $(".show-mockup-dg").toggle();
-        //$(".mockup-dg").toggle();
-    });
-    $(".show-mockup-dg").click(function(){
-        $(".show-mockup-dg").toggle();
-        //$(".mockup-dg").toggle();
-    });
+//    $(".mockup-dg").click(function(){
+//        $(".show-mockup-dg").toggle();
+//        //$(".mockup-dg").toggle();
+//    });
+//    $(".show-mockup-dg").click(function(){
+//        $(".show-mockup-dg").toggle();
+//        //$(".mockup-dg").toggle();
+//    });
+//    
+//    $(".mockup-sm").click(function(){
+//        $(".show-mockup-sm").toggle();
+//        //$(".mockup-sm").toggle();
+//    });
+//    $(".show-mockup-sm").click(function(){
+//        $(".show-mockup-sm").toggle();
+//        //$(".mockup-sm").toggle();
+//    });
+    
+    
+    
     
     $(".mockup-sm").click(function(){
-        $(".show-mockup-sm").toggle();
-        //$(".mockup-sm").toggle();
+        $(".show-mockup-sm").css("display", "block");
+        $(".show-mockup-dg").css("display", "none");
     });
-    $(".show-mockup-sm").click(function(){
-        $(".show-mockup-sm").toggle();
-        //$(".mockup-sm").toggle();
+    $(".mockup-dg").click(function(){
+        $(".show-mockup-dg").css("display", "block");
+        $(".show-mockup-sm").css("display", "none");
     });
 });
